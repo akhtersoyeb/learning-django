fe3dce016e4e82abb84dea1a614efc93a522be77
soyeb <soyeb@pop-os.localdomain>
2021-10-02 18:59:48 +0530

    completed day 6

--- ecommerceProject/app/models.py
+++ ecommerceProject/app/models.py
@@ -1,3 +1,69 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User 
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+STATE_CHOICES = (
+    ('Assam', 'Assam'), 
+    ('Bihar', 'Bihar'),
+    ('Chandigarh', 'Chandigarh'), 
+    ('West Bengal', 'West Bengal')
+)
+
+class Customer(models.Model): 
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=150)
+    locality = models.CharField(max_length=150)
+    city = models.CharField(max_length=150)
+    zipcode = models.IntegerField() 
+    state = models.CharField(choices=STATE_CHOICES, max_length=150)
+
+    def __str__(self): 
+        return self.name
+
+CATEGORY_CHOICES = (
+    ('M', 'Mobile'), 
+    ('L', 'Laptop'), 
+    ('TW', 'Top Wear'), 
+    ('BW', 'Bottor Wear')
+)
+
+class Product(models.Model): 
+    title = models.CharField(max_length=150)
+    selling_price = models.FloatField() 
+    discounted_price = models.FloatField() 
+    description = models.TextField() 
+    brand = models.CharField(max_length=150)
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=5)
+    product_image = models.ImageField(upload_to='productImg')
+
+    def __str__(self): 
+        return self.title 
+
+class Cart(models.Model): 
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self): 
+        return str(self.id)
+
+STATUS_CHOICES = (
+    ('Pending', 'Pending'), 
+    ('Packed', 'Packed'), 
+    ('On The Way', 'On The Way'), 
+    ('Delivered', 'Delivered'), 
+    ('Cancel', 'Cancel'), 
+)
+
+
+class OrderPlaced(models.Model): 
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    ordered_date = models.DateTimeField(auto_now_add=True) 
+    status = models.CharField(choices=STATUS_CHOICES, max_length=50, 
+                                default='Pending') 
+
+
--- ecommerceProject/app/views.py
+++ ecommerceProject/app/views.py
@@ -1,10 +1,37 @@
 from django.shortcuts import render
 
-def home(request):
- return render(request, 'app/home.html')
+from .models import Customer, Product, Cart, OrderPlaced
 
-def product_detail(request):
- return render(request, 'app/productdetail.html')
+from django.views import View 
+
+# def home(request):
+#  return render(request, 'app/home.html')
+
+# HOME View
+class ProductView(View):
+    def get(self, request): 
+        topwears = Product.objects.filter(category = 'TW')
+        bottomwears  = Product.objects.filter(category='BW')
+        mobiles = Product.objects.filter(category='M')
+
+        context = {
+            'topwears': topwears, 
+            'bottomwears': bottomwears, 
+            'mobiles' : mobiles
+        }
+        return render(request, 'app/home.html', context)
+
+# def product_detail(request):
+#  return render(request, 'app/productdetail.html')
+
+
+class ProductDetailView(View): 
+    def get(self, request, pk):
+        product = Product.objects.get(id=pk)
+        context = {
+            'product': product
+        }
+        return render(request, 'app/productdetail.html', context)
 
 def add_to_cart(request):
  return render(request, 'app/addtocart.html')
