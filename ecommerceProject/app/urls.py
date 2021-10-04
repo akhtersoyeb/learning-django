1696d93b957451fe414461553c70b85a72373279
soyeb <soyeb@pop-os.localdomain>
2021-10-04 20:52:04 +0530

    day 7 completed

--- ecommerceProject/app/urls.py
+++ ecommerceProject/app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from app import views
+from . import views
 
 from django.conf import settings
 from django.conf.urls.static import static 
+
+from .forms import LoginForm
+from django.contrib.auth import views as auth_view
 
 
 urlpatterns = [
@@ -11,12 +14,24 @@ urlpatterns = [
     path('product-detail/<int:pk>/', views.ProductDetailView.as_view(), name='product-detail'),
     path('cart/', views.add_to_cart, name='add-to-cart'),
     path('buy/', views.buy_now, name='buy-now'),
-    path('profile/', views.profile, name='profile'),
+    # path('profile/', views.profile, name='profile'),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
     path('address/', views.address, name='address'),
     path('orders/', views.orders, name='orders'),
     path('changepassword/', views.change_password, name='changepassword'),
-    path('mobile/', views.mobile, name='mobile'),
-    path('login/', views.login, name='login'),
-    path('registration/', views.customerregistration, name='customerregistration'),
+
+    path('mobile/', views.mobile, name='mobile'), # /mobiles
+    path('mobile/<slug:data>/', views.mobile, name='mobiledata'), #/mobiles/Redmi
+
+    # path('login/', views.login, name='login'),
+    # path('registration/', views.customerregistration, name='customerregistration'),
+    path('accounts/login/', auth_view.LoginView.as_view(
+        template_name='app/login.html', 
+        authentication_form =LoginForm
+        ), name='login'), 
+    path('registration/', views.CustomerRegistrationView.as_view(), name='customerregistration'), 
+    path('logout/', auth_view.LogoutView.as_view(
+        next_page='login'
+    ), name='logout'),
     path('checkout/', views.checkout, name='checkout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ecommerceProject/app/views.py
+++ ecommerceProject/app/views.py
@@ -1,23 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from .forms import CustomerRegistrationForm, CustomerProfileForm
 
 from .models import Customer, Product, Cart, OrderPlaced
 
-from django.views import View 
+from django.views import View
 
 # def home(request):
 #  return render(request, 'app/home.html')
 
 # HOME View
+
+
 class ProductView(View):
-    def get(self, request): 
-        topwears = Product.objects.filter(category = 'TW')
-        bottomwears  = Product.objects.filter(category='BW')
+    def get(self, request):
+        topwears = Product.objects.filter(category='TW')
+        bottomwears = Product.objects.filter(category='BW')
         mobiles = Product.objects.filter(category='M')
 
         context = {
-            'topwears': topwears, 
-            'bottomwears': bottomwears, 
-            'mobiles' : mobiles
+            'topwears': topwears,
+            'bottomwears': bottomwears,
+            'mobiles': mobiles
         }
         return render(request, 'app/home.html', context)
 
@@ -25,7 +29,7 @@ class ProductView(View):
 #  return render(request, 'app/productdetail.html')
 
 
-class ProductDetailView(View): 
+class ProductDetailView(View):
     def get(self, request, pk):
         product = Product.objects.get(id=pk)
         context = {
@@ -33,32 +37,102 @@ class ProductDetailView(View):
         }
         return render(request, 'app/productdetail.html', context)
 
+
 def add_to_cart(request):
- return render(request, 'app/addtocart.html')
+    return render(request, 'app/addtocart.html')
+
 
 def buy_now(request):
- return render(request, 'app/buynow.html')
+    return render(request, 'app/buynow.html')
 
-def profile(request):
- return render(request, 'app/profile.html')
+
+# def profile(request):
+#     return render(request, 'app/profile.html')
+
+
+class ProfileView(View): 
+    def get(self, request): 
+        form = CustomerProfileForm() 
+        context = {
+            'form': form 
+        }
+        return render(request, 'app/profile.html', context)
+
+    def post(self, request):  
+        form = CustomerProfileForm(request.POST)
+        if form.is_valid() : 
+            user = self.request.user 
+            # print (user.username)
+            name = form.cleaned_data['name']
+            locality = form.cleaned_data['locality']
+            city = form.cleaned_data['city']
+            state = form.cleaned_data['state']
+            zipcode = form.cleaned_data['zipcode']
+
+            new_customer = Customer(
+                user=user ,
+                name=name, 
+                locality=locality, 
+                city=city, 
+                state=state, 
+                zipcode=zipcode
+            )
+            new_customer.save() 
+        return redirect('address')
+
+            
 
 def address(request):
- return render(request, 'app/address.html')
+    add = Customer.objects.filter(user=request.user)
+    context = {
+        'add': add
+    }
+    return render(request, 'app/address.html', context)
+
 
 def orders(request):
- return render(request, 'app/orders.html')
+    return render(request, 'app/orders.html')
+
 
 def change_password(request):
- return render(request, 'app/changepassword.html')
+    return render(request, 'app/changepassword.html')
 
-def mobile(request):
- return render(request, 'app/mobile.html')
 
-def login(request):
- return render(request, 'app/login.html')
+def mobile(request, data=None): 
+    if data == None: 
+        mobiles = Product.objects.filter(category='M')
+    elif data == 'Redmi' or data == 'Samsung': 
+        mobiles = Product.objects.filter(category='M').filter(brand=data)
+    context = {
+        'mobiles': mobiles
+    }
+    return render(request, 'app/mobile.html', context)
 
-def customerregistration(request):
- return render(request, 'app/customerregistration.html')
+
+# def login(request):
+#     return render(request, 'app/login.html')
+
+
+# def customerregistration(request):
+#     return render(request, 'app/customerregistration.html')
+
+class CustomerRegistrationView(View): 
+    def get(self, request): 
+        form = CustomerRegistrationForm() 
+        context = {
+            'form': form 
+        }
+        return render(request, 'app/customerregistration.html', context)
+
+    def post(self, request): 
+        form = CustomerRegistrationForm(request.POST)
+        if form.is_valid() : 
+            form.save() 
+         
+        return redirect('login')
+
+# form base 
+# class base
 
 def checkout(request):
- return render(request, 'app/checkout.html')
+    return render(request, 'app/checkout.html')
